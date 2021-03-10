4b990f5177188b825162e4971a391fd392068f7f
Daniel <daniel_sousa.p@outlook.com>
2021-03-10 00:35:09 -0300

    feat: implements socketio to nodejs project

--- app.ts
+++ app.ts
@@ -1,8 +1,29 @@
 import express from 'express';
 import routes from './routes';
+import http from 'http';
+import io, { Socket } from 'socket.io';
+import cors from 'cors';
 
 const app = express();
+const serverHttp = http.createServer(app);
+const socketServer = new io.Server(serverHttp, {
+  cors: {
+    origin: '*',
+  },
+});
+
+let messages: any = [];
+
+socketServer.on('connection', (socket: Socket) => {
+  console.log('new connection', socket.id);
+  socket.on('msg', (id: string, msg: string) => {
+    messages.push({ id, msg });
+    socketServer.sockets.emit('msg', messages);
+  });
+});
 
 app.use(routes);
 
-app.listen(3333, () => console.log('Server runing in PORT 3333'));
+app.use(cors());
+
+serverHttp.listen(3333, () => console.log('Serving running in PORT 3333'));
